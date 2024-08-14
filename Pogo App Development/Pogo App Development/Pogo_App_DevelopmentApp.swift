88843fa735419b98d58627868944c7736d2b9d5f
Ivebens Eliacin <143521981+Schmoneous@users.noreply.github.com>
2024-08-13 23:17:59 -0400

    Add files via upload
    This is the newest version of the app, which correctly sends information to the esp32 via Bluetooth

---
+++ Pogo App Development/Pogo App Development/Pogo_App_DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pogo_App_DevelopmentApp.swift
+//  Pogo App Development
+//
+//  Created by Ivebens Eliacin  on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Pogo_App_DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pogo App Development/Pogo App Development/Views/Manual.swift
@@ -0,0 +1,16 @@
+//
+//  Manual.swift
+//  Pogo App Development
+//
+//  Created by Ivebens Eliacin  on 8/13/24.
+//
+
+import SwiftUI
+struct Manual: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+#Preview {
+    Manual()
+}
